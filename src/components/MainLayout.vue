0b6c3906631531e1bc9dd5183183a4126977c519
ffeduus <merendi.federica@gmail.com>
2021-06-03 16:34:34 +0200

    Main layout and Bootstrap and D3 import

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <p> Hello There!</p>
+  <b-container>
+    <b-row id="firstRow" >
+      <b-col id="userData">
+        User data is going here
+      </b-col>
+      <b-col id="map">
+        The map is going here
+      </b-col>
+    </b-row>
+  </b-container>
+</div>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+}
+
+
+</script>
+
+<style scoped>
+#map, #userData {
+  background-color: bisque;
+  margin: 1px;
+}
+</style>
